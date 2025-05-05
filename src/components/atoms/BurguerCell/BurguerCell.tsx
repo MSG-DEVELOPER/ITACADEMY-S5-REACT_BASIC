c414380d19d5eadf50d3572c07db4c6d4fefd6d5
Joker <puebloseko@gmail.com>
2025-05-05 18:29:23 +0200

    primer break point 1024px se cambia de navbar normal a hamburguesa.Le añadimos funcionalidad del click, ya muestra componente.

---
+++ src/components/atoms/BurguerCell/BurguerCell.tsx
@@ -0,0 +1,10 @@
+import css from './BurguerCell.module.css'
+
+
+function BurguerCell() {
+  return (
+    <div>BurguerCell</div>
+  )
+}
+
+export default BurguerCell
---
+++ src/components/molecules/BurguerMenu/BurguerMenu.tsx
@@ -0,0 +1,10 @@
+import css from './BurgerMenu.module.css'
+
+
+function BurguerMenu() {
+  return (
+    <div>BurguerMenu</div>
+  )
+}
+
+export default BurguerMenu
